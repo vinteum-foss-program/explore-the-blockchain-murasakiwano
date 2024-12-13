d6ccccf08673702a61113e037d0ca89d35434732
Johann Homonnai <j.homonnai@icloud.com>
2024-12-13 18:12:30 -0300

    ref(004.sh): use awk to strip quotes from checksum

--- submission/004.sh
+++ submission/004.sh
@@ -2,7 +2,7 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 
 checksum=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)" | jq .checksum)
-checksum=${checksum//\"/}
+checksum=$(echo "$checksum" | awk '{gsub(/^\"/, ""); gsub(/\"$/, ""); print}')
 
 descriptor="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)#$checksum"
 
