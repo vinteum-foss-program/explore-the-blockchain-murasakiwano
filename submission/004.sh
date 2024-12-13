c370165da0e9c5aac866faebe3ad92b974b93aab
Johann Homonnai <j.homonnai@icloud.com>
2024-12-13 18:05:38 -0300

    Add submission/004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,9 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+checksum=$(bitcoin-cli getdescriptorinfo "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)" | jq .checksum)
+checksum=${checksum//\"/}
+
+descriptor="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)#$checksum"
+
+bitcoin-cli deriveaddresses "$descriptor" | jq .[]
