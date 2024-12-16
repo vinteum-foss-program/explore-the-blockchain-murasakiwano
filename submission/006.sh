3439413e4737e871f1199c49a128363939640018
Johann Homonnai <j.homonnai@icloud.com>
2024-12-16 18:16:14 -0300

    add submission/006.sh

--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,32 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+gettransaction() {
+  rawtx=$(bitcoin-cli getrawtransaction $1)
+
+  bitcoin-cli decoderawtransaction $rawtx | jq .
+}
+
+blockhash=$(bitcoin-cli getblockhash 256128)
+coinbasetxid=$(
+  bitcoin-cli getblock $blockhash |
+    jq ".tx.[0]" | # First transaction in a block is the coinbase tx
+    awk '{gsub(/^\"/, ""); gsub(/\"$/, ""); print}'
+)
+
+blockhash=$(bitcoin-cli getblockhash 257343)
+txids=$(bitcoin-cli getblock $blockhash | jq ".tx.[]")
+
+for txid in ${txids[@]}; do
+  tx=$(gettransaction $(echo $txid | awk '{gsub(/"/, ""); print}'))
+  txvin=$(echo $tx | jq '.vin.[] | .txid')
+
+  for txvinid in ${txvin[@]}; do
+    txvinid=$(echo $txvinid | awk '{gsub(/"/, ""); print}')
+
+    if [[ $txvinid = $coinbasetxid ]]; then
+      break 2
+    fi
+  done
+done
+
+echo $txvinid
