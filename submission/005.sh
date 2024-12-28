358e9382f76b776472b53d58712e37013a04c031
Johann Homonnai <j.homonnai@icloud.com>
2024-12-28 11:23:44 -0300

    fix(submission/005.sh): use compact jq output

--- submission/005.sh
+++ submission/005.sh
@@ -4,6 +4,6 @@
 txid="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
 raw_tx=$(bitcoin-cli getrawtransaction $txid)
 
-pubkeys=$(bitcoin-cli decoderawtransaction $raw_tx | jq '.vin | map(.txinwitness.[1])')
+pubkeys=$(bitcoin-cli decoderawtransaction $raw_tx | jq -c '.vin | map(.txinwitness.[1])')
 
-bitcoin-cli createmultisig 1 $pubkeys | jq ".address"
+bitcoin-cli createmultisig 1 $pubkeys | jq -r ".address"
